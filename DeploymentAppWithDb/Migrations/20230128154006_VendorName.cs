3de6a3f5536df8778c48233c83aebbfbf4024f3d
Chandru <jchandru@gmail.com>
2023-01-28 18:20:27 -0600

    Add project files.

---
+++ DeploymentAppWithDb/Migrations/20230128154006_VendorName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeploymentAppWithDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class VendorName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
